793e0cf619bd807eea530b8d943df8d2d456edbd
Friend <101312044+SahilS1G@users.noreply.github.com>
2023-05-29 15:59:20 +0530

    Add files via upload

---
+++ server/key/key.go
@@ -0,0 +1,3 @@
+package key
+
+const Api_key = "aabf9b6bf5ed43e39dfffb9a171982b3"
---
+++ server/router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"github.com/SahilS1G/server/controllers"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.Use(mux.CORSMethodMiddleware(router))
+	router.HandleFunc("/getPositive", controllers.GetPositveNews).Methods("GET")
+	router.HandleFunc("/getNegative", controllers.GetNegativeNews).Methods("GET")
+	router.HandleFunc("/company", controllers.GetNews).Methods("GET")
+	router.HandleFunc("/search", controllers.HandleSearch).Methods("POST")
+	return router
+}
